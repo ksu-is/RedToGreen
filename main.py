fe1f9ad6545a35837ab3aa33af4a871109bdee59
jmilne10 <jmilne10@students.kennesaw.edu>
2026-04-26 19:02:44 -0400

    Added CSV structure for trade storage

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("Welcome to Red to Green Tracker")
+import csv
+
+FILENAME = "trades.csv"
+
+def add_trade():
+    ticker = input("Enter ticker: ")
+    entry = float(input("Entry price: "))
+    exit = float(input("Exit price: "))
+
+    pnl = (exit - entry) * 100
+
+    print("PnL:", pnl)
+
+def main():
+    print("Red to Green Tracker")
+
+    choice = input("Press 1 to add trade: ")
+
+    if choice == "1":
+        add_trade()
+
+main()
